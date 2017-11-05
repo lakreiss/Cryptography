1b9749c7cb974086dfa66a36349d14b8bf0e03e4
Liam Kreiss <liamkreiss@Liams-MacBook-Pro.local>
2017-11-05 12:47:56 -0800

    added some documentation

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ import java.util.Collections;
  */
 public class Main {
 
+    //English uses alphabet: a b c d e f g h i j k l m n o p q r s t u v w x y z
+    //Spanish uses alphabet: a b c d e f g h i j k l m n o p q r s t u v w x y z á é í ñ ó ú ü
+
     public static final boolean RANDOM_SHIFT = false;
     public static final int NUM_BEST_SOLUTIONS = 10;
 
@@ -13,8 +16,8 @@ public class Main {
         Language english = new English();
         Language spanish = new Spanish();
 
-        String spanishSentence = "Tengo hambre. Quiero comer un burrito. yo fui a la parcela de calabazas ayer";
-        String englishSentence = "I am hungry. I want to eat a burrito. I went to the pumpkin patch yesterday";
+        String spanishSentence = "Tengo hambre. Quiero comer un burrito. yo fui a la parcela de calabazas ayer. Tengo una tortuga mascota";
+        String englishSentence = "I am hungry. I want to eat a burrito. I went to the pumpkin patch yesterday. I have a pet turtle";
 
         Decrypter englishDecrypter = new Decrypter(english);
         Decrypter spanishDecrypter = new Decrypter(spanish);
@@ -23,7 +26,8 @@ public class Main {
         String encryptedSpanishSentence = caesarEncrypt(spanish,  spanishSentence, 6);
 
         //choose language
-        String encryptedSentence = encryptedSpanishSentence;
+        //This simulates how a real message would be encrypted (English messages would be encrypted in English)
+        String encryptedSentence = encryptedEnglishSentence;
 
         ArrayList<String> encryptedMessagesEnglish = new ArrayList<String>();
         encryptedMessagesEnglish.add(encryptedSentence);
@@ -43,6 +47,13 @@ public class Main {
         System.out.println(getNBestSolutions(NUM_BEST_SOLUTIONS, allSolutionsList, encryptedSentence));
     }
 
+    /**
+     * Caesar Encypts a message MESSAGE based on a language LANGUAGE with letter shift N
+     * @param language
+     * @param message
+     * @param n
+     * @return
+     */
     public static String caesarEncrypt(Language language, String message, int n) {
         if (RANDOM_SHIFT) {
             return new CaesarCipherEncrypter(language).encrypt(message);
@@ -51,6 +62,13 @@ public class Main {
         }
     }
 
+    /**
+     * returns the original message and the N best solutions in a formatted String
+     * @param numBestSolutions
+     * @param allSolutionLists
+     * @param origEncryptedMessage
+     * @return
+     */
     private static String getNBestSolutions(int numBestSolutions, ArrayList<ArrayList<Solution>> allSolutionLists, String origEncryptedMessage) {
         String output = String.format("Original Encrypted Message: %s\n", origEncryptedMessage);
         ArrayList<Solution> allSolutions = new ArrayList<>();
